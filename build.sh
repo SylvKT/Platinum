515ea3e7993c977c40730a66be94d8b78f71513c
TehcJS <tehcdev@gmail.com>
2020-08-16 12:03:25 -0400

    folder auto-generator

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
+# TODO: add linux support
+# NOTE: i haven't tested it yet, but i think this will still work on linux since it doesn't reqiure any platform-specific frameworks/libraries
+
+# check if folders exist
+# if not, create them
+folders=( "build" "build/win" "build/osx" "build/linux" )
+for i in "${folders[@]}"
+do
+	if [ ! -d "$i" ]
+	then
+		mkdir "$i"
+	fi
+done
+
 # build static library
-g++ -c -o build/platinum.o src/include/platinum.cpp
-ar rvs build/platinum.a build/platinum.o
+g++ -std=c++98 -c src/include/Platinum/lib.cpp -o build/libplatinum.o
+ar rvs build/libplatinum.a build/libplatinum.o
 
 # build shared library
+
+# build test
+g++ -std=c++98 example/test.cpp -o build/osx/test -Isrc/include -Lbuild -lplatinum -lglfw -framework OpenGL
